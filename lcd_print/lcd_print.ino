bc509ba671e0b65036c469ac2bb89e4657d54d7f
Noel Piña <124635877+ImTheSrPina@users.noreply.github.com>
2024-06-05 19:17:42 -0600

    Add files via upload

---
+++ lcd_print/lcd_print.ino
@@ -0,0 +1,25 @@
+
+#include <LiquidCrystal_I2C.h>  // download from https://github.com/ELECTROALL/Codigos-arduino/blob/master/LiquidCrystal_I2C.zip
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+void setup() {
+  // put your setup code here, to run once:
+    lcd.init();
+    lcd.backlight();
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("<-- Evasora de"); 
+    lcd.setCursor (0,1);
+    lcd.print("<-- Impuestos");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    lcd.display();
+    delay(500);
+    lcd.noDisplay();
+    delay(500);
+
+}
